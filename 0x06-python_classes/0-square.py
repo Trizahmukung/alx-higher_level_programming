26f48a8bbe3e8e37b502230ca246a5f65e538b20
Trizah Mukung <mukungtrizah@gmail.com>
2022-07-08 15:44:46 +0000

    Write an empty class Square

--- 0x06-python_classes/0-square.py
+++ 0x06-python_classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-
-=======
 """Empty class Square that defines a square"""
 
 
 class Square:
     """Empty class Square that defines a square"""
     pass
->>>>>>> cd956a2e11d026bfe70b5cefdc1cc342289028ff
